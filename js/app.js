ff356b1bd4effe4db3191ecb86dff484388733bd
ajay7231 <53226413+ajay7231@users.noreply.github.com>
2020-08-20 17:57:54 +0530

    dark mode

--- js/app.js
+++ js/app.js
@@ -16,3 +16,10 @@ var currentScrollPos = window.pageYOffset;
   }
   prevScrollpos = currentScrollPos;
 }
+const btn = document.querySelector('.btn-toggle');
+
+// Listen for a click on the button
+btn.addEventListener('click', function() {
+  // Then toggle (add/remove) the .dark-theme class to the body
+  document.body.classList.toggle('dark-theme');  
+})
