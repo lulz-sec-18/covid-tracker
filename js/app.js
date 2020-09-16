54960c8f005fd65a333f19bdf253a2e2b035c2f3
ajay7231 <53226413+ajay7231@users.noreply.github.com>
2020-09-16 18:09:40 +0530

    login

--- js/app.js
+++ js/app.js
@@ -6,18 +6,7 @@ $(document).scroll(function(){
 });
 
 
-/*var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-var currentScrollPos = window.pageYOffset;
-if (prevScrollpos > currentScrollPos) {
-  document.getElementsByClassName("navbar").style.top = "0";
-} else {
-  
-  document.getElementsByClassName("navbar").style.transition = "1s";
-}
-prevScrollpos = currentScrollPos;
-}
-*/
+
 const btn = document.querySelector('.btn-toggle');
 
 // Listen for a click on the button
@@ -31,11 +20,11 @@ $(document).ready(function(){
 var flag = 0;  
 $("input.btn-toggle").click(function(){
   if(flag == 0) {
-    $(".logo-src").attr("src","images/logo.svg");
+    $(".logo-src").attr("src","images/logo_white.svg");
     flag = 1;
   }
   else if(flag == 1) {
-    $(".logo-src").attr("src","images/logo_white.svg");
+    $(".logo-src").attr("src","images/logo.svg");
     flag = 0;
   }
 });
@@ -55,11 +44,14 @@ signInButton.addEventListener('click', () => {
 container.classList.remove("right-panel-active");
 });
 
-document.getElementById('button').addEventListener('click', function(){
-document.querySelector('.popup').style.display = 'flex';
-});
+// document.getElementsById('userButton').addEventListener('click', function(){ // show form by changing display property
+// document.querySelector('.popup').style.display = 'flex';
+// });
+function showForm(){
+  document.querySelector('.popup').style.display = 'flex';
+}
 
-document.getElementById('close').addEventListener('click', function(e){
+document.getElementById('close').addEventListener('click', function(e){ // closes form on clicking cross
 
 document.querySelector('.popup').style.display='none';
 
@@ -69,34 +61,54 @@ document.getElementById('close2').addEventListener('click', function(e){
 document.querySelector('.popup').style.display='none';
 
 });
+//on submit event functions
+function signup(){
+  document.querySelector('.popup').style.display='none';//hide form
+ 
+  let head = "Sign Up Successful"
+  let body_text = "You are subscribed to daily corona updates"
+  createCustomNotification(head,body_text) // notification on signing up
+
+}
+function signin(name){
+  document.querySelector('.popup').style.display='none';//hide form
+ 
+  let head = "Sign in Successful"
+  let body_text = "Welcome  to covitrax " + name;
+  createCustomNotification(head,body_text) // notification on signing in
+
+}
+
 
 //Notification
 
   function removeNotifications(){
-  const notifications = document.querySelectorAll('.visible')
+  const notifications = document.querySelectorAll('.visible') // All elements with visible class
   if(notifications.length >0){
-    notifications.forEach((noti) =>{
+    notifications.forEach((noti) =>{ 
       noti.classList.add('remove')
       setTimeout(() =>{
-        noti.remove();
+        noti.remove(); // removing element
       }, 300);
     })
   }
 }
 
-function createNotification(){
+//notification div maker function
+
+function createCustomNotification(head,body_text){
   let noti = document.createElement('div')
-  let h3 = document.createElement('h3')
+  let h3 = document.createElement('h3')//creates Elements
   let p = document.createElement('p')
-  h3.innerHTML = "Welcome to CoviTrax"
-  p.innerHTML = 'Here you will get overall cencus of Covid-19'
-  noti.className = 'notification'
+  h3.innerHTML = head
+  p.innerHTML = body_text
+  noti.className = 'notification'//Add Classes
   h3.className = 'noti-head'
   p.className = 'noti-body'
-  noti.appendChild(h3)
+  noti.appendChild(h3) //appending to parent element
   noti.appendChild(p)
   document.body.appendChild(noti)
-  removeNotifications()
+  removeNotifications() //hiding After timeout
   setTimeout(() => {
     noti.classList.add('visible')
   }, 10);
@@ -109,11 +121,170 @@ function createNotification(){
   }, 2500);
   
 }
-createNotification();
+let onLoadHead = "Welcome to CoviTrax"
+let onLoadBody = 'Here you will get overall cencus of Covid-19' // variable stored content
+createCustomNotification(onLoadHead,onLoadBody)
+
+function boto () {
+  let js = document.createElement('script');
+  js.type = 'text/javascript';
+  js.async = 1;
+  js.src = 'https://go.botmaker.com/rest/webchat/p/74NGS7JADrcb/init.js?l=en&t=w';
+  document.body.appendChild(js);
+}
+boto();
+
+// User Authentication--------------------------------------------------------------------->
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+
+    document.getElementById("user_div").style.display = "block";
+    document.getElementById("login_div").style.display = "none";
+
+    var user = firebase.auth().currentUser;
+
+    if(user != null){
+
+      var email_id = user.email;
+      document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
+
+    }
+
+  } else {
+    // No user is signed in.
+
+    document.getElementById("user_div").style.display = "none";
+    document.getElementById("login_div").style.display = "block";
+
+  }
+});
+
+ function login(){
+
+var userEmail = document.getElementById("email_field").value;
+var userPass = document.getElementById("password_field").value;
+console.log("login pressed");
+var flag = 1;
+
+firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+  
+// // //     // Handle Errors here.
+
+var errorCode = error.code;
+ var errorMessage = error.message;
+
+ window.alert("Error : " + errorMessage);
+ flag = 0;
+
+// ...
+ });
+ 
+
+}
+
+function logout(){
+  firebase.auth().signOut();
+}
 
 
 
+// new code starts here
 
 
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+
+    // document.getElementById("ser_div").style.display = "block";
+    // document.getElementById("ogin_div").style.display = "none";
+
+    var user = firebase.auth().currentUser;
+
+    if(user != null){
+
+      var email_id = user.email;
+      // document.getElementById("ser_para").innerHTML = "Welcome User : " + email_id;
+
+    }
+
+  } else {
+    // No user is signed in.
+
+    // document.getElementById("ser_div").style.display = "none";
+    // document.getElementById("ogin_div").style.display = "block";
+
+  }
+});
+
+function ogin(){
+
+  var userEmail = document.getElementById("mail_field").value;
+  var userPass = document.getElementById("assword_field").value;
+
+  firebase.auth().createUserWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    window.alert("Error : " + errorMessage);
+
+    // ...
+  });
+
+}
+
+function logout(){
+  firebase.auth().signOut();
+}
+
+
+// google signin
+
+googleSignIn = () => {
+  base_provider = new firebase.auth.GoogleAuthProvider()
+  firebase.auth().signInWithPopup(base_provider).then(function(result){
+    console.log(result)
+    console.log("success google account linked")
+    var user = result.user;
+    signin(user.displayName);
+  }).catch(function(err){
+    console.log(err)
+    console.log("Failed to do")
+  })
+}
+
+
+// facebook sign in
+
+// {
+//   status: 'connected',
+//   authResponse: {
+//       accessToken: '...',
+//       expiresIn:'...',
+//       signedRequest:'...',
+//       userID:'...'
+//   }
+// }
+
+// function checkLoginState() {
+//   FB.getLoginStatus(function(response) {
+//     statusChangeCallback(response);
+//   });
+// }
+
+facebookSignIn = () => {
+  base_provider = new firebase.auth.FacebookAuthProvider()
+  
+  firebase.auth().signInWithPopup(base_provider).then(function(result){
+    console.log(result)
+    console.log("success facebook account linked")
+    signin(result.displayName)
+  }).catch(function(err){
+    console.log(err)
+    console.log("Failed to do")
+  })
+}
 
 
