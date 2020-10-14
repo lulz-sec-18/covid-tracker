76dcc8eda80adf61f6c105377cf5f81799f1b81e
ajay7231 <53226413+ajay7231@users.noreply.github.com>
2020-10-14 20:07:15 +0530

    faq

--- js/app.js
+++ js/app.js
@@ -376,3 +376,17 @@ $(function () {
 $(function () {
   $("#draggable-prof").draggable(); //draggable jquery
 });
+
+
+$(function() {
+	$('.accordion').find('.accordion__title').click(function(){
+		// Adds active class
+		$(this).toggleClass('active');
+		// Expand or collapse this panel
+		$(this).next().slideToggle('fast');
+		// Hide the other panels
+		$('.accordion__content').not($(this).next()).slideUp('fast');
+		// Removes active class from other titles
+		$('.accordion__title').not($(this)).removeClass('active');		
+	});
+});
