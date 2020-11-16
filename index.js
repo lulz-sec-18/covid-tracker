210168f3fc79dabb4820d35fc98b22e35a45ef5d
mridul-netizen <mridulsetia18@gmail.com>
2020-11-16 12:29:23 +0530

    updated

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http');
 //const path = require('path');
 const app = express();
 //const router = express.Router();
-const port = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 const server = http.Server(app);
 
 //app.use(express.static('public'));
@@ -17,9 +17,9 @@ const server = http.Server(app);
 
 app.use(express.static(__dirname + '/public'));
 
-server.listen(port, err => {
+server.listen(PORT, err => {
     if (err) {
       return console.log("ERROR", err);
     }
-    console.log(`Listening on port ${port}`);
+    console.log(`Listening on port ${PORT}`);
   });
